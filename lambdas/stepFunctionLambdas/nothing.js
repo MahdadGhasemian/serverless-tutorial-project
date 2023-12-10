f34ccf9db893962cf6551ea733301b8b35518259
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2023-12-10 11:21:28 +0330

    FunctionName

--- lambdas/stepFunctionLambdas/nothing.js
+++ lambdas/stepFunctionLambdas/nothing.js
@@ -1,12 +1,5 @@
-const Dynamo = require("../common/Dynamo")
-const { v4: uuidv4 } = require('uuid');
-
-const tableName = process.env.signupTableName;
-
 exports.hander = async event => {
     console.log("event", event)
 
-    const ok = true;
-
-    return { ok }
+    return;
 }
